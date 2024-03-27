1f215d20255c8154cef0758965751c6b3e49cfa0
Mainak Halder <mainakhalder5598633@gmail.com>
2024-03-27 14:04:32 +0530

    27 mar.cpp

--- 2024/3.mar/27mar.cpp
+++ 2024/3.mar/27mar.cpp
@@ -1,19 +1,98 @@
-// https://leetcode.com/problems/subarray-product-less-than-k/submissions/1215231789/
+//https://www.geeksforgeeks.org/problems/find-shortest-safe-route-in-a-matrix/1
 
 
-class Solution {
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int left = 0, right = 0, product = 1, count = 0;
-    int n = nums.size();
-    if(k <= 1) return 0;
-    while (right < n) {
-      product *= nums[right];
-      while (product >= k) product /= nums[left++];
-      count += 1 + (right - left);
-      right++;
+    bool check(int i, int j, vector<vector<int>>&mat){
+        if(i-1>=0 && mat[i-1][j]==0) return false;
+        if(i+1<mat.size() && mat[i+1][j]==0) return false;
+        if(j-1>=0 && mat[i][j-1]==0) return false;
+        if(j+1<mat[0].size() && mat[i][j+1]==0) return false;
+        
+        return true;
     }
-    return count;
+    
+    int solve(int i, int j, vector<vector<int>>&mat, vector<vector<int>>&vis, vector<vector<int>>&dp){
+        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size() || vis[i][j]==1 || mat[i][j]==0) return INT_MAX;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(!check(i,j,mat)) return INT_MAX;
+        
+        if(j==0) return mat[i][0];
+        
+        vis[i][j]=true;
+        
+        int left=solve(i, j-1, mat, vis, dp);
+    
+        int up=solve(i-1, j, mat, vis, dp);
+        
+        int down=solve(i+1, j, mat, vis, dp);
+        
+        if(left!=INT_MAX) left+=mat[i][j];
+        if(up!=INT_MAX) up+=mat[i][j];
+        if(down!=INT_MAX) down+=mat[i][j];
+        
+        vis[i][j]=0;
+        
+        return dp[i][j]=min(left, min(up, down));
+    }
+
+    int findShortestPath(vector<vector<int>> &mat)
+    {
+       // code here
+       int n=mat.size();
+       int m=mat[0].size();
+       vector<vector<int>>vis(n, vector<int>(m,0));
+       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+       int mini=INT_MAX;
+       for(int i=0;i<n;i++){
+           mini=min(mini, solve(i, m-1, mat, vis,dp));
+       }
+       
+       if(mini==INT_MAX) return -1;
+       else return mini;
        
     }
 };
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int R, C;
+        cin >> R >> C;
+        vector<vector<int>> mat(R, vector<int>(C));
+        for (int i = 0; i < R; i++)
+        {
+            for (int j = 0; j < C; j++)
+            {
+                cin >> mat[i][j];
+            }
+        }
+
+        Solution ob;
+        int ans = ob.findShortestPath(mat);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
